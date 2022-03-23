4b3fbc5331ab13dd982a1e6bc2bdda4a1e6bf0c3
CHAPMAN Peter <Peter.CHAPMAN@dhsoha.state.or.us>
2022-03-23 16:29:57 -0700

    shipping/billing

--- src/app/components/merchandise/checkout/checkout.component.ts
+++ src/app/components/merchandise/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { CheckoutformService } from 'src/app/services/checkoutform.service';
 
 @Component({
@@ -8,14 +10,15 @@ import { CheckoutformService } from 'src/app/services/checkoutform.service';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-  [x: string]: any;
 
   checkoutFormGroup: FormGroup;
-
   totalPrice: number = 0;
   totalQuantity: number = 0;
   creditcardYears: number[]=[];
   creditcardMonths: number[]=[];
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
   
   constructor(private formBuilder: FormBuilder, private checkoutFormService: CheckoutformService) { }
 
@@ -53,19 +56,27 @@ export class CheckoutComponent implements OnInit {
 
     //populate credit card months and years
     const startMonth: number = new Date().getMonth()+1;
+
     this.checkoutFormService.getCreditCardMonths(startMonth).subscribe(
       data => {
         console.log("Retrieved credit card months " +JSON.stringify(data));
         this.creditcardMonths=data;
       }
-    ) 
+    ); 
 
     this.checkoutFormService.getCreditCardYears().subscribe(
       data => {
         console.log("Retrieved credit card years: " +JSON.stringify(data));
         this.creditcardYears=data;
       }
-    )
+    );
+    //populate Countries
+    this.checkoutFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries "+JSON.stringify(data));
+        this.countries = data;
+      }
+    );
   }
 
   copyShippingAddressToBillingAddress(event) {
@@ -76,8 +87,54 @@ export class CheckoutComponent implements OnInit {
     }
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+    }    
+  }
+
+  handleMonthsAndYears(){
+    const creditcardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const currentYear:number = new Date().getFullYear();
+    const selectedYear: number = Number(creditcardFormGroup.value.expirationYear);
+    //if current year = selected year, start with current month
+    let startMonth: number;
+    if(currentYear === selectedYear){
+      startMonth = new Date().getMonth()+1;
     }
-    
+    else{
+      startMonth = 1;
+    }
+    this.checkoutFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: "+ JSON.stringify(data));
+        this.creditcardMonths = data;
+      }
+    )
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log("FormGroupName: "+formGroupName);
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.checkoutFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data; 
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
   }
 
   onSubmit() {
--- src/app/services/checkoutform.service.ts
+++ src/app/services/checkoutform.service.ts
@@ -1,31 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutformService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
 
-getCreditCardMonths(startMonth: number):Observable<number[]>{
-  let data: number[]=[];
-  //build array for dropdown month
-  for(let month = startMonth; month <= 12; month++){
-    data.push(month);
+  constructor(private httpClient: HttpClient) {}
+
+    getCountries(): Observable<Country[]> {
+      return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+        map(response => response._embedded.countries)
+      );
+    }
+
+    getStates(theCountryCode: string): Observable<State[]> {
+
+      // search url
+      const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+  
+      return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+        map(response => response._embedded.states)
+      );
+    }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+    //build array for dropdown month
+    for (let month = startMonth; month <= 12; month++) {
+      data.push(month);
+    }
+    return of(data);
   }
-  return of(data);
-}
 
-getCreditCardYears(): Observable<number[]>{
-  //for credit card year dropdown. start at current year and get next 10 years
-  let data:number[]=[];
-  const startYear:number = new Date().getFullYear();
-  const endYear:number = startYear+10;
-  for(let year=startYear; year<=endYear; year++){
-    data.push(year);
+  getCreditCardYears(): Observable<number[]> {
+    //for credit card year dropdown. start at current year and get next 10 years
+    let data: number[] = [];
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+    for (let year = startYear; year <= endYear; year++) {
+      data.push(year);
+    }
+    return of(data);
   }
-  return of(data);
 }
 
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[]
+  }
 }
+
+  interface GetResponseStates {
+    _embedded: {
+      states: State[];
+    }
+  }
+
+
