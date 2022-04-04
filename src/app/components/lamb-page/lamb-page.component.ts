8204aa98f1bedfded8e28579cb9b43317309e2e3
CHAPMAN Peter <Peter.CHAPMAN@dhsoha.state.or.us>
2022-04-04 06:22:00 -0700

    css updates

--- src/app/components/lamb-page/lamb-page.component.ts
+++ src/app/components/lamb-page/lamb-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-lamb-page',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LambPageComponent implements OnInit {
 
+  produceFormGroup: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
